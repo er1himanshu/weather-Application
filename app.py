deaddc23fb4608f9e7b87b5a0ccad65ae365a6b7
er1himanshu <er.himanshusoni.cse@gmail.com>
2024-04-20 13:16:25 +0530

    my app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request  
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -6,18 +6,21 @@ app = Flask(__name__)
 @app.route("/")
 def home_page():
     return render_template("index.html")
-@app.route("/weather_apps", methods = ["POST","GET"])      # ("/name") here name should be same as in html file <action> part name
-def get_data():
-    url = "https://api.openweathermap.org/data/2.5/weather"
-    param = {
-            'q' : request.form.get("city"),
-            'appid' : request.form.get("appid"),
-            'units': request.form.get("units")
-            } 
-    response = requests.get(url,params = param) 
-    data = response.json()
-    return f"data : {data}"
 
+@app.route("/weather_apps", methods=["POST", "GET"])
+def get_data():
+    if request.method == "POST":
+        url = "https://api.openweathermap.org/data/2.5/weather"
+        params = {
+            'q': request.form.get("city"),
+            'appid': request.form.get("appid"),
+            'units': request.form.get("units")
+        }
+        response = requests.get(url, params=params)
+        data = response.json()
+        return render_template("weather_result.html", data=data)
+    else:
+        return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 5002)
+    app.run(host="0.0.0.0", port=5002)
